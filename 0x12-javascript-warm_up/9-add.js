630c4ad7798c5dcf11a5d1e34485546cb73b7feb
denimwenda <denimwendwa@gmail.com>
2023-06-19 13:21:20 -0700

    changes

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  const c = a + b;
+  console.log(c);
 }
 
-const firstArgument = process.argv[2];
-const secondArgument = process.argv[3];
-
-const num1 = parseInt(firstArgument);
-const num2 = parseInt(secondArgument);
-
-const result = add(num1, num2);
-console.log(result)
+add(Number(process.argv[2]), Number(process.argv[3]));
