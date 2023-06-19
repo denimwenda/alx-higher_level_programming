f264e7967445aa53ca7b9bc0d92ceac548da8633
denimwenda <denimwendwa@gmail.com>
2023-06-19 13:17:03 -0700

    changes

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+const num1 = parseInt(firstArgument);
+const num2 = parseInt(secondArgument);
+
+const result = add(num1, num2);
+console.log(result)
